7a80763ae7d30e8be65dea0342ea6124cc7143ce
Chiijlaw <chiijlaw@berkeley.edu>
2018-06-27 18:19:43 -0700

    finished up to once

--- src/underbar.js
+++ src/underbar.js
@@ -241,11 +241,25 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.each(arguments, function(ele){
+      _.each(ele, function(value, key){
+        obj[key] = value;
+      });
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(ele){
+      _.each(ele, function(value, key){
+        if (obj[key] === undefined) {
+          obj[key] = value;
+        }
+      });
+    });
+    return obj;
   };
 
 
